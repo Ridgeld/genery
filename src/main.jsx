255ec9b1ee2f62ce34b88e9c44dcfc01dee6592a
Daniel <rayl.alim.2014@gmail.com>
2024-04-02 21:42:48 +0600

    cor

--- src/main.jsx
+++ src/main.jsx
@@ -19,9 +19,7 @@ const router = createBrowserRouter({ routes });
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
