229caf9beece14b7774c20848048b462f6854fba
Daniel <rayl.alim.2014@gmail.com>
2024-04-02 23:06:17 +0600

    test_9

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
 ]);
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("main")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
