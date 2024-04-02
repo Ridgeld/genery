eecada9dbd10c05af2effc42bd51b963eff5963b
Daniel <rayl.alim.2014@gmail.com>
2024-04-02 23:20:20 +0600

    test_1

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Добавлены Routes и Route
 
-const router = createBrowserRouter([
+// Определите маршруты внутри компонента App или внутри Routes
+const routes = [
   {
     path: "",
     element: <div>Hello world!</div>,
   },
-]);
-
+];
 
 ReactDOM.createRoot(document.getElementById("main")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+        <Route path="/app" element={<App />} /> {/* Пример маршрута для компонента App */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
