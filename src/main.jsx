d3fc460b53a4d37ae9fb0b161bf8e2e4d79e575c
Daniel <rayl.alim.2014@gmail.com>
2024-04-03 17:17:02 +0600

    last_last_test

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-import { HashRouter, createBrowserRouter } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
-    element: <h2>Привет!</h2>
+    element: <h2>Привет!</h2>,
   },
   {
-    path:'/about-us',
-    element: <App/>
-  }
-])
-
+    path: "/about-us",
+    element: <App/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('main')).render(
   <React.StrictMode>
     <div className="wrapper">
       <div className="content">
-      <HashRouter router={router}/>
+      <RouterProvider router={router} />
       <h2>6565</h2>
       </div>
     </div>
