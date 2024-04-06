b2b265457422c3f0521a3413b6aa0dca45825198
Daniel <rayl.alim.2014@gmail.com>
2024-04-06 14:22:26 +0600

    chng

--- src/about/image_container/Image_container.jsx
+++ src/about/image_container/Image_container.jsx
@@ -2,8 +2,8 @@ import styles from './Image_container.module.scss'
 export function Image_container(){
   return (
     <div className={styles.container}>
-        <img src='/img_1.png' className={styles.img_1}  />
-        <img src='/img_2.png' className={styles.img_2}  />
+        <img src='./img_1.png' className={styles.img_1}  />
+        <img src='./img_2.png' className={styles.img_2}  />
     </div>
   )
 }
