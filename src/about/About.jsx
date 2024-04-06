d3dfcaf25c24fcb856ee6fa5b7cfdc5f7021dac8
Daniel <rayl.alim.2014@gmail.com>
2024-04-06 14:11:16 +0600

    chng_1

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,6 +1,7 @@
 import { Header } from '../header/Header.jsx'
 import { Image_container } from './image_container/Image_container.jsx'
 import { MySlider } from './slider/Slider.jsx'
+
 export function About(){
   return (
     <>
