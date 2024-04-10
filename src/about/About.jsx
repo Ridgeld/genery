f33d5d0a7060122f61d9b0bb00863b6a24590aa9
Daniel <rayl.alim.2014@gmail.com>
2024-04-10 13:11:30 +0600

    test_3

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -4,9 +4,10 @@ import { Image_container } from './image_container/Image_container.jsx'
 import { MySlider } from './slider/Slider.jsx'
 
 export function About(){
+  const isMain = true;
   return (
     <>
-    <Header isMain={true}/>
+    <Header isMain={isMain}/>
     <MySlider/>
     <Buttons/>
     <Image_container/>
