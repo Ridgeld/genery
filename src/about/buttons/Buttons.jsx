7b83a57625aba4599976e15760291aa97d88f55d
Daniel <rayl.alim.2014@gmail.com>
2024-04-11 16:39:54 +0600

    test

--- src/about/buttons/Buttons.jsx
+++ src/about/buttons/Buttons.jsx
@@ -1,8 +1,17 @@
 import styles from './Buttons.module.scss'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react';
+
 export function Buttons(){
+  const navigate = useNavigate();
+  useEffect(() => {
+      navigate('/logo');
+
+    return () => clearTimeout(timeoutId);
+  }, [navigate]);
   return (
     <div className={styles.container}>
-        <button className={`${styles.button} ${styles.stroke}`}>Регистрация</button>
+        <button className={`${styles.button} ${styles.stroke}`} onClick={useEffect}>Регистрация</button>
         <button className={`${styles.button} ${styles.fill}`}>Войти</button>
     </div>
   )
