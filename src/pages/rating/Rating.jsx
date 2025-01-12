3c2e5727885e9914a527ad4ef6f4a4f85ffec75e
Daniel <danielmustafin076@gmail.com>
2025-01-12 20:51:39 +0600

    Service update 1.1

--- src/pages/rating/Rating.jsx
+++ src/pages/rating/Rating.jsx
@@ -56,7 +56,7 @@ export default function Rating(){
 
     return(
         <div className={styles['container']}>
-            <iframe className={styles['game']} src="https://students-rating-private-git-master-ridgelds-projects.vercel.app/"></iframe>
+            <iframe className={styles['game']} src="https://students-rating-private-ridgelds-projects.vercel.app/"></iframe>
         </div>
     )
 }
